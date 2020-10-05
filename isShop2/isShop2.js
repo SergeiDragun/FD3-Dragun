b8692a347da1d59d189a4017103314a2bab3f32d
sergei_dragun <siarhei.drahun@gmail.com>
2020-10-05 23:00:14 +0300

    1st commit

---
+++ isShop2/isShop2.js
@@ -0,0 +1,33 @@
+let IsShop2 = React.createClass({
+
+    displayName: "IsShop",
+
+    getDefaultProps: function() {
+       return { shop: "Какой-то магазин" }
+    },
+
+    render: function() {
+        let products = this.props.productList.map(item => {
+            return React.DOM.tr( {key: item.code},
+                React.DOM.td( null, item.name ),
+                React.DOM.td( null, React.DOM.img({src: item.url}) ),
+                React.DOM.td( null, item.price ),
+                React.DOM.td( null, item.balance),
+            );
+
+        });
+
+        return React.DOM.table( {className:'IsShop'}, 
+            React.DOM.caption( {className:'ShopName'}, this.props.shop ),
+            React.DOM.thead( {className: "Options"},
+                React.DOM.tr( null, 
+                    React.DOM.th( null, "Подукт"),
+                    React.DOM.th( null, "Изображение"),
+                    React.DOM.th( null, "Цена"),
+                    React.DOM.th( null, "Отаток"),
+                ) 
+            ),
+            React.DOM.tbody( {className:'Products'}, products),
+        );
+    },
+})
