7bf8876767388c7041e2cab0f06014aca1388a2c
sergei_dragun <siarhei.drahun@gmail.com>
2020-10-28 15:20:24 +0300

    1st build

--- RainbowFrame/bundle.js
+++ RainbowFrame/bundle.js
@@ -29164,8 +29164,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = __webpack_require__(2);
 
 var _react2 = _interopRequireDefault(_react);
@@ -29176,38 +29174,59 @@ var _propTypes2 = _interopRequireDefault(_propTypes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var RainbowFrame = function RainbowFrame(props) {
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+    var length = props.colors.length;
+    var index = -1;
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var RainbowFrame = function (_React$component) {
-    _inherits(RainbowFrame, _React$component);
-
-    function RainbowFrame() {
-        _classCallCheck(this, RainbowFrame);
-
-        return _possibleConstructorReturn(this, (RainbowFrame.__proto__ || Object.getPrototypeOf(RainbowFrame)).apply(this, arguments));
-    }
-
-    _createClass(RainbowFrame, [{
-        key: 'render',
-        value: function render() {
+    var renderFrames = function renderFrames(colors) {
+        length -= 1;
+        index += 1;
+        if (length == 0) {
             return _react2.default.createElement(
                 'div',
-                null,
-                this.props.colors
+                { style: { border: "solid 10px " + colors[index], padding: "10px" } },
+                props.children
             );
         }
-    }]);
+        return _react2.default.createElement(
+            'div',
+            { style: { border: "solid 10px " + colors[index], padding: "10px" } },
+            renderFrames(colors)
+        );
+    };
 
-    return RainbowFrame;
-}(_react2.default.component);
+    return renderFrames(props.colors);
+};
 
 RainbowFrame.propTypes = {
-    colors: _propTypes2.default.array.isRequired
+    colors: _propTypes2.default.arrayOf(_propTypes2.default.string)
 };
+
+/* class RainbowFrame extends React.Component {
+
+    static propTypes = {
+        colors: PropTypes.arrayOf(
+            PropTypes.string.isRequired
+        )
+    }
+
+    length = this.props.colors.length;
+    index = -1;
+    renderFrames =(colors) => {
+        this.length-=1;
+        this.index+=1;
+        if (this.length == 0) {
+            return <div style={{border:"solid 10px "+colors[this.index],padding:"10px"}}>{this.props.children}</div>
+        }
+        return <div style={{border:"solid 10px "+colors[this.index],padding:"10px"}}>{this.renderFrames(colors)}</div>
+    }
+    
+    render() {
+        return this.renderFrames(this.props.colors);
+    }
+} */
+
 exports.default = RainbowFrame;
 
 /***/ }),
